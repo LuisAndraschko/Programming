9e4f58f7bc4fbfe1d760ddf0b029d025f8a0892f
LuisAndraschko <luis_andraschko@hotmail.com>
2022-02-24 17:14:38 -0300

    class 3/15 finished - forms and user input

--- python/Python Flask Tutorial Full-Featured Web App Part 1/Flask_Blog/flaskblog.py
+++ python/Python Flask Tutorial Full-Featured Web App Part 1/Flask_Blog/flaskblog.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, url_for
+from turtle import title
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+import os
 
 app = Flask(__name__)
+
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 posts = [
         {
@@ -25,7 +31,31 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title='About')
+    return render_template('about.html', 
+                           title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        (flash(f'Account created for {form.username.data}!', 'success'))
+        return redirect(url_for('home')) 
+        
+    return render_template('register.html', 
+                           title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@gmail.com' and form.password.data == 'password':
+            (flash(f'Loggeg in as {form.email.data}.', 'success'))
+            return redirect(url_for('home'))
+        else:
+            (flash(f'Could not find a match, try again.', 'danger'))
+    return render_template('login.html', 
+                           title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True) 
